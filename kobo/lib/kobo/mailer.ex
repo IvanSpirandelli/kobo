28511ab4af642dccbb4f0a035ff4a5952ba7dc59
IvanSpirandelli <ivan@spirandelli.de>
2022-12-13 16:20:16 +0100

    moving docker-compose.yml up

--- kobo/lib/kobo/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Kobo.Mailer do
-  use Swoosh.Mailer, otp_app: :kobo
-end
--- kobo/lib/kobo/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Kobo.Repo do
-  use Ecto.Repo,
-    otp_app: :kobo,
-    adapter: Ecto.Adapters.Postgres
-end
